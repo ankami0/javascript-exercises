a5e09e14d00f06a2732a1555dda618a169a2438b
N L <204485432+ankami0@users.noreply.github.com>
2025-04-02 13:26:49 +0200

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    if (!Number.isInteger(from) || !Number.isInteger(to) || from < 0 || to < 0)
+        return "ERROR";
+    let result = 0;
+    for (let i=Math.min(from,to);i<=Math.max(from,to);i++)
+        result+=i;
+    return result;
 };
 
 // Do not edit below this line
