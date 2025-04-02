5a7a9b97e5e8a19aa081dc9c47ab6650460de5a7
N L <204485432+ankami0@users.noreply.github.com>
2025-04-02 13:16:49 +0200

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = [];
+    for (let considered of array) {
+        let keep = true;
+        for (let toDelete of args) {
+            if (considered === toDelete) {
+                keep = false;
+                break;
+            }
+        }
+        if (keep)
+            result.push(considered);
+    }
+    return result;
 };
 
 // Do not edit below this line
